52643600f5ba31519668d3a59d74f034c0e2e1a9
Sebastian Concepcion <sebadames123@icloud.com>
2024-02-08 13:52:12 -0400

    Update files

--- modulo01/lab02/app.py
+++ modulo01/lab02/app.py
@@ -1,4 +1,6 @@
 mensaje = input('Ingrese su nombre: ')
+gpa = float(input('Ingrese su GPA: '))
+edad = int(input('Ingrese su edad: '))
 
-
-print(f'Hola {mensaje}!!!')
+print(f'Hola {mensaje}, tu GPA es: {gpa}')
+print(f'Tu edad es: {edad}')
