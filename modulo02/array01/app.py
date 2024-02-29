d371ed48c2e80560992e8df29449b094c20af949
Sebastian Concepcion <sebadames123@icloud.com>
2024-02-29 14:26:44 -0400

    Update

---
+++ modulo02/array01/app.py
@@ -0,0 +1,16 @@
+import array
+
+aNumber = array.array('i')
+
+for i in range(1, 4): #from 1 to 3
+    number = int(input('Enter a number: '))
+    aNumber.append(number)
+
+for i in range(3): #from 0 to 2
+    print(f'Indice {i} - valor {aNumber[i]}')
+
+for n in aNumber:
+    print(f'Valor {n}')
+
+for i in range(len(aNumber)):
+    print(f'Indice {i} - valor {aNumber[i]}')
